b2e771129041216a1bb86872d705fb8f127d93f3
Kalpit Shah <72689203+kalpitshah2311@users.noreply.github.com>
2022-02-12 21:25:24 +0530

    Practical 6 done

---
+++ wordOrder.py
@@ -0,0 +1,18 @@
+#20ce129 Kalpit Shah Practical 6
+
+import collections;
+
+N = int(input())
+d = collections.OrderedDict()
+
+for i in range(N):
+    word = input()
+    if word in d:
+        d[word] +=1
+    else:
+        d[word] = 1
+
+print(len(d));
+
+for k,v in d.items():
+    print(v,end = " ");
